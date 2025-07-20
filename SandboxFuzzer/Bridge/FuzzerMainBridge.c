d273ac42346ef9ba45020c19dfe3c853cdc85909
DragonXmodz1569 <Dragonxmodz>
2025-07-20 02:37:04 +0100

    Update:

--- SandboxFuzzer/Bridge/FuzzerMainBridge.c
+++ SandboxFuzzer/Bridge/FuzzerMainBridge.c
@@ -6,7 +6,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     atomic_fetch_add(&gFuzzIterationCount, 1);
     // your fuzzing code...
     return 0;
-}
+}yes
 
 int FuzzerMain(int argc, char **argv) {
     extern int LLVMFuzzerRunDriver(int *argc, char ***argv,
