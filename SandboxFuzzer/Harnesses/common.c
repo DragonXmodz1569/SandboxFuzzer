2a7150b6dcafd9cbb92cee71ab7b7cc4c56254e6
DragonXmodz1569 <Dragonxmodz>
2025-07-20 09:21:23 +0100

    Update:

--- SandboxFuzzer/Harnesses/common.c
+++ SandboxFuzzer/Harnesses/common.c
@@ -1,2 +1,7 @@
 // Harnesses/common.c
 #include <stdatomic.h>
+#include <CFNetwork/CFHTTPMessage.h>
+
+void RTSPParserProcess(CFHTTPMessageRef msg) {
+    (void)msg; // suppress unused warning
+}
--- SandboxFuzzer/common.h
+++ SandboxFuzzer/common.h
@@ -1,17 +1,8 @@
+#include <stdatomic.h>
+
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdatomic.h>
+extern _Atomic uint64_t gFuzzIterationCount;
 
-// Shared atomic counter (defined once in FuzzerMainBridge.c)
-extern _Atomic(uint64_t) gFuzzIterationCount;
-
-// Each harness’s entrypoint prototype
-int harness_imtranscoder_test(const uint8_t *data, size_t size);
-int harness_appstored_test(const uint8_t *data, size_t size);
-int harness_airplay_rtsp_test(const uint8_t *data, size_t size);
-// …add one prototype per harness you implement…
-
-#endif // COMMON_H
+#endif
